f17804050c7bcd215930d3a1484762f244ad4036
Schenkel <thi.schenkel@gmail.com>
2022-07-08 17:45:10 +0200

    debug

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,7 +34,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-
+            $this->addFlash('success', "Inscription validée! Vous pouvez à présent vous connecter ci-dessous.");
             return $this->redirectToRoute('app_home');
         }
 
